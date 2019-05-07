0292f1e9394d998f8ed0a5ac60be1ed69f75f010
jimpanseiscool42 <j2001b@gmail.com>
2019-05-07 13:28:05 +0200

    upload files

---
+++ js/game.js
@@ -0,0 +1,73 @@
+var fbRef = new Firebase("https://gameforgamer69.firebaseio.com/data");
+
+var otherPlayers = {};
+
+var playerID;
+var player;
+
+function loadGame() {
+    // load the enviroment
+    loadEnvironment();
+    // load the player
+    initMainPlayer();
+
+    listenToOtherPlayers();
+
+    window.onunload = function() {
+        fbRef.child( "Players" ).child( playerID ).remove();
+    }
+
+    window.onbeforeunload = function() {
+        fbRef.child( "Players" ).child( playerID ).remove();
+    }
+}
+
+function listenToPlayer( playerData ) {
+    if ( playerData.val() ) {
+        otherPlayers[playerData.key()].setOrientation( playerData.val().orientation.position, playerData.val().orientation.rotation );
+    }
+}
+
+function listenToOtherPlayers() {
+    // when a player is added, do something
+    fbRef.child( "Players" ).on( "child_added", function( playerData ) {
+        if ( playerData.val() ) {
+            if ( playerID != playerData.key() && !otherPlayers[playerData.key()] ) {
+                otherPlayers[playerData.key()] = new Player( playerData.key() );
+                otherPlayers[playerData.key()].init();
+                fbRef.child( "Players" ).child( playerData.key() ).on( "value", listenToPlayer );
+            }
+        }
+    })
+    // when a player is removed, do something
+
+    fbRef.child( "Players" ).on( "child_removed", function( playerData ) {
+        if ( playerData.val() ) {
+
+        }
+    });
+}
+
+
+
+function initMainPlayer() {
+
+    playerID = fbRef.child( "Players" ).push().key();
+
+    fbRef.child( "Players" ).child( playerID ).child("orientation").set({
+        position: {x: 0, y:0, z:0},
+        rotation: {x: 0, y:0, z:0}
+    });
+
+    player = new Player( playerID );
+    player.isMainPlayer = true;
+    player.init();
+}
+
+function loadEnvironment() {
+    var sphere_geometry = new THREE.SphereGeometry( 1 );
+    var sphere_material = new THREE.MeshNormalMaterial();
+    var sphere = new THREE.Mesh( sphere_geometry, sphere_material );
+
+    scene.add( sphere );
+}
---
+++ js/main.js
@@ -0,0 +1,51 @@
+var container, scene, camera, renderer;
+
+var controls;
+
+init();
+animate();
+
+function init() {
+    // Setup
+    container = document.getElementById( "container" );
+
+    scene = new THREE.Scene();
+
+    camera = new THREE.PerspectiveCamera( 50, window.innerWidth / window.innerHeight, 1, 100 );
+    camera.position.z = 5;
+
+    renderer = new THREE.WebGLRenderer( { alpha: true } );
+    renderer.setSize( window.innerWidth, window.innerHeight);
+
+    // Load game world
+    loadGame();
+
+    // Events
+    window.addEventListener( "resize", onWindowResize, false );
+
+    container.appendChild( renderer.domElement );
+    document.body.appendChild( container );
+}
+
+function animate() {
+    requestAnimationFrame( animate );
+
+    if ( controls ) {
+        controls.update();
+    }
+    
+    render();
+}
+
+function render() {
+
+    renderer.clear();
+    renderer.render( scene, camera );
+}
+
+function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize( window.innerWidth, window.innerHeight );
+}
